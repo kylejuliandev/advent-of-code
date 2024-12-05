f705e0354ed4f0d77488e95df3dc2c1dd8d3bf50
Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
2024-12-05 21:03:01 +0000

    Add day 5 part one 2024

---
+++ Advent2024/Day5/Puzzle.cs
@@ -0,0 +1,104 @@
+﻿namespace Advent2024.Day5;
+
+public class Puzzle
+{
+    public int GetSumOfMiddleNumbers(string pageOrderingRules, string updates)
+    {
+        var nodes = GetNodes(pageOrderingRules);
+        var sum = 0;
+
+        foreach (var update in updates.Split(Environment.NewLine))
+        {
+            var pages = update.Split(',').Select(int.Parse).ToArray();
+            
+            if (!nodes.TryGetValue(pages[0], out var node))
+            {
+                continue;
+            }
+
+            var correctOrder = true;
+            for (var i = 0; i < pages.Length - 1; i++)
+            {
+                var nextPage = pages[i + 1];
+
+                var nextNode = node.Children.FirstOrDefault(n => n.Value == nextPage);
+                if (nextNode != null)
+                {
+                    node = nextNode;
+                }
+                else
+                {
+                    correctOrder = false;
+                    break;
+                }
+            }
+
+            if (correctOrder)
+            {
+                var midPoint = pages.Length / 2;
+                var mid = pages[midPoint];
+
+                sum += mid;
+            }
+        }
+
+        return sum;
+    }
+
+    private static Dictionary<int, Node> GetNodes(string pageOrderingRules)
+    {
+        var rules = pageOrderingRules.Split(Environment.NewLine);
+        var nodes = new Dictionary<int, Node>();
+        foreach (var rule in rules)
+        {
+            var options = rule.Split('|');
+            var left = int.Parse(options[0]);
+            var right = int.Parse(options[1]);
+
+            if (nodes.TryGetValue(left, out var node))
+            {
+                if (nodes.TryGetValue(right, out var linkedNode))
+                {
+                    node.Children.Add(linkedNode);
+                }
+                else
+                {
+                    var newNode = new Node(right);
+                    nodes.Add(right, newNode);
+                    node.Children.Add(newNode);
+                }
+            }
+            else
+            {
+                if (nodes.TryGetValue(right, out var linkedNode))
+                {
+                    var newNode = new Node(left);
+                    nodes.Add(left, newNode);
+                    newNode.Children.Add(linkedNode);
+                }
+                else
+                {
+                    var leftNode = new Node(left);
+                    var rightNode = new Node(right);
+                    nodes.Add(left, leftNode);
+                    nodes.Add(right, rightNode);
+                    leftNode.Children.Add(rightNode);
+                }
+            }
+        }
+
+        return nodes;
+    }
+
+    public class Node
+    {
+        public int Value { get; }
+
+        public List<Node> Children { get; } = [];
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
--- Advent2024/Program.cs
+++ Advent2024/Program.cs
@@ -1,16 +1,50 @@
-﻿using Advent2024.Day4;
+﻿using Advent2024.Day5;
 
 Console.WriteLine("Enter your Puzzle input. Press CTRL+Z when complete");
 
-using var sr = new StreamReader(Console.OpenStandardInput(), Console.InputEncoding);
-var consoleInput = sr.ReadToEnd();
-var input = consoleInput.Trim();
+//using var sr = new StreamReader(Console.OpenStandardInput(), Console.InputEncoding);
+//var consoleInput = sr.ReadToEnd();
+//var input = consoleInput.Trim();
+
+var rules =
+    """
+    47|53
+    97|13
+    97|61
+    97|47
+    75|29
+    61|13
+    75|53
+    29|13
+    97|29
+    53|29
+    61|53
+    97|53
+    61|29
+    47|13
+    75|47
+    97|75
+    47|61
+    75|61
+    47|29
+    75|13
+    53|13
+    """;
+
+var updates =
+    """
+    75,47,61,53,29
+    97,61,53,29,13
+    75,29,13
+    75,97,47,61,53
+    61,13,29
+    97,13,75,29,47
+    """;
+
+//var rules = File.ReadAllText("Day5/PageRules.txt");
+//var updates = File.ReadAllText("Day5/Updates.txt");
 
 var puzzle = new Puzzle();
-var result = puzzle.NumberOfXmasWords(input);
+var result = puzzle.GetSumOfMiddleNumbers(rules, updates);
 
 Console.WriteLine("Program result: {0}", result);
-
-var result2 = puzzle.NumberOfX_MASWords(input);
-
-Console.WriteLine("Program result 2: {0}", result2);
